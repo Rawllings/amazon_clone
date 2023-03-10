fec284a39ab30e73e982ef4f5b1c8c941c044290
Rawllings Kiprop <rutojr01@gmail.com>
2023-03-10 23:26:24 +0300

    Done with login form desing, next is functionality to make it responsive

--- amazon_clone/src/Login.js
+++ amazon_clone/src/Login.js
@@ -15,6 +15,22 @@ function Login() {
 
       <div className="login__container">
         <h1>Sign-in</h1>
+        <form className="login__form">
+          <h5>Email</h5>
+          <input type="text" />
+
+          <h5>Password</h5>
+          <input type="password" />
+
+          <button className="login__signInButton">Sign In</button>
+        </form>
+        <p>
+          By continuing, you agree to Amazon's Conditions of Use and Privacy
+          Notice.
+        </p>
+        <button className="login__registerButton">
+          Create Your Amazon Account
+        </button>
       </div>
     </div>
   );
